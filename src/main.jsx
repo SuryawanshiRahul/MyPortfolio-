8bf6981a0dfd8d808e882615c3316e8a51e5387f
SuryawanshiRahul <suryawanshirahul9623@gmail.com>
2026-07-10 08:15:23 +0530

    Remove HelmetProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App";
 
@@ -19,8 +18,6 @@ import "./assets/css/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>
 );
